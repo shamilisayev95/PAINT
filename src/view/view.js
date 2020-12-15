d47d89453048bb6992f2cd429b64eef287e39b05
AleksandrSavelyev <punk558721973@gmail.com>
2020-12-15 10:14:49 +0300

    save

--- src/view/view.js
+++ src/view/view.js
@@ -1,14 +1,9 @@
 class View {
   constructor() {
-    this.root = null;
-    this.canvas = null;
-    this.footerRange = null;
-    this.footerColor = null;
-    this.mainContainer = null;
-    this.footerContainer = null;
+    
   }
   init = () => {
-    this.root = document.getElementById('root');
+
   }
 
 
